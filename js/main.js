bb7a90e12171b993d0ae793dfd036116b74c8af1
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-08-10 17:43:45 +0100

    Debugged API to display movies properly

--- js/main.js
+++ js/main.js
@@ -7,11 +7,33 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
+  console.log(searchText);
   // make a call to api using axios
   axios
-    .get("http://www.omdbapi.com/?apikey=8455bd77&?s=" + searchText)
+    .get("http://www.omdbapi.com/?s=" + searchText + "&apikey=8455bd77")
     .then(response => {
       console.log(response);
+      // add search result to array
+      let movies = response.data.Search;
+      let output = "";
+      // jquery for each
+      $.each(movies, (index, movie) => {
+        // output movies
+        console.log(movies);
+        output += `
+            <div class="col-md-3">
+                <div class="well text-center">
+                    <img src="${movie.Poster}">
+                    <h5>${movie.Title}</h5>
+                    <a onclick="movieSelected('${
+                      movie.imdbID
+                    }')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+            </div>
+          `;
+      });
+      console.log(output);
+      $("#movies").html(output);
     })
     .catch(err => {
       console.log(err);
